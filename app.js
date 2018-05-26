30e719deca193686b23d02e0a1e26a211732b6cb
peesam <39553458+peesam@users.noreply.github.com>
2018-05-26 16:09:34 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var orderCount=0;
+
+function updateOrders() {
+	document.getElementById("orders").innerHTML=orderCount;
+}
+
+var orderCount=0;
+
+function updateOrders() {
+	document.getElementById("orders").innerHTML=orderCount;
+}
+
+
+
+
+
